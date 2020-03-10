620d5a67152693dcb5f4012bd7a6f2474606bf3c
chris-dai <hi@chrisdai.com>
2020-03-10 10:16:52 +0000

    cri

--- algo.py
+++ algo.py
@@ -4,72 +4,87 @@ from PIL import Image
 import numpy as np
 from random_dots import random_dots
 
-# import an image as grayscale 2D array
-left = cv2.imread('RandomA.png', 0).tolist()
-right = cv2.imread('RandomB.png', 0).tolist()
-
-
 def matching_cost(L, R):
-    mean = int((L + R) // 2)
-    return ((mean - L) * (mean - R)) // 2
+    mean = int((L + R) / 2)
+    result = ((mean - L) * (mean - R)) / 16
+    return abs(result)
 
 
 def cost_matrix(L, R):
-    occlusion = 3.8
-    cost_mat = [[0] * (len(L) + 1) for _ in range(len(R) + 1)]
-    path = []
 
-    for i in range(len(cost_mat)):
-        cost_mat[i][0] = i * occlusion
-        cost_mat[0][i] = i * occlusion
+  occlusion = 2.66
+  # initialise matrix
+  cost_mat = [[0]*(len(R)+1) for _ in range(len(L)+1)]
 
-    for i in range(len(cost_mat)):
-        for j in range(len(cost_mat)):
-            case_1 = cost_mat[i - 1][j - 1] + matching_cost(L[i - 1], R[j - 1])
-            case_2 = cost_mat[i - 1][j] + occlusion
-            case_3 = cost_mat[i][j - 1] + occlusion
-            cost_mat[i][j] = min(case_1, case_2, case_3)
+  # initiate first col
+  for i in range(len(cost_mat)):
+    cost_mat[i][0] = i*occlusion
 
-            if cost_mat[i][j] == case_1:
-                path.append([-1, -1])
-            elif cost_mat[i][j] == case_2:
-                path.append([-1, 0])
-            else:
-                path.append([0, -1])
+  # initiate first col
+  for j in range(len(cost_mat[0])):
+    cost_mat[0][j] = j*occlusion
 
-    # back tracking
-    back_i = len(cost_mat) - 1
-    back_j = len(cost_mat[0]) - 1
+  def matching_cost(l, r):
+    return (l-r)**2//16
 
-    result = []
+  for i in range(1, len(cost_mat)):
+    for j in range(1, len(cost_mat[0])):
+      case_1 = cost_mat[i-1][j-1] + matching_cost(L[i-1], R[j-1])
+      case_2 = cost_mat[i-1][j] + occlusion
+      case_3 = cost_mat[i][j-1] + occlusion
+      cost_mat[i][j] = min(case_1, case_2, case_3)
 
-    while back_i > 0 and back_j > 0:
-        case_1 = cost_mat[back_i - 1][back_j - 1] + matching_cost(L[back_i - 1], R[back_j - 1])
-        case_2 = cost_mat[back_i][back_j - 1] + occlusion
-        case_3 = cost_mat[back_i - 1][back_j] + occlusion
+  # backtrack
+  back_i = len(cost_mat)-1
+  back_j = len(cost_mat[0])-1
 
-        if cost_mat[back_i][back_j] == case_1:
-            result.append([back_i - 1, back_j - 1, 255])
-            back_i -= 1
-            back_j -= 1
-        elif cost_mat[back_i][back_j] == case_2:
-            result.append([back_i, back_j - 1, 0])
-            back_j -= 1
-        else:
-            result.append([back_i - 1, back_j, 0])
-            back_i -= 1
+  res = []
+  # TODO: length not fixed for res
+  while back_i>0 and back_j>0:
+    case_1 = cost_mat[back_i-1][back_j-1] + matching_cost(L[back_i-1], R[back_j-1])
+    case_2 = cost_mat[back_i-1][back_j] + occlusion
+    case_3 = cost_mat[back_i][back_j-1] + occlusion
 
-    result = result[::-1]
-    return result    # [i, j, RGB]
+    if cost_mat[back_i][back_j] == case_1:
+      # res.append(abs(L[back_i-1]-R[back_j-1]) + 128)
+      if(back_i == back_j):
+        res.append(128)
+      else:
+        res.append(abs(L[back_i-1]-R[back_j-1]) + 188)
+      back_i -= 1
+      back_j -= 1
+    elif cost_mat[back_i][back_j] == case_2:
+      res.append(0)
+      back_i -= 1
+    else:
+      # path.append([back_i, back_j-1])
+      res.append(0)
+      back_j -= 1
 
-# create two random dot images
-random_dots()
+  res = res[::-1]
+  return res
 
-res_matrix = []
+# imgA = Image.open("RandomA.png")
+# left = np.asarray(imgA)
+# imgB = Image.open("RandomB.png")
+# right = np.asarray(imgB)
+
+left = cv2.imread('view1.png', 0).tolist()
+right = cv2.imread('view2.png', 0).tolist()
+
+result_matrix = []
 for row in range(len(left)):
-    L = left[row]
-    R = right[row]
-    res_matrix.append(cost_matrix(L, R))
-plt.imshow(res_matrix)
-plt.show()
+  L = left[row]
+  R = right[row]
+  result_matrix.append(cost_matrix(L, R))
 
+
+res = []
+for row in result_matrix:
+  if len(row) > 417:
+    res.append(row[:417])
+  else:
+    res.append(row + [0]*(417-len(row)))
+
+plt.imshow(res, cmap="gray")
+plt.show()
